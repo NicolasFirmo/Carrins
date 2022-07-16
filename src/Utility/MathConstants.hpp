f2358380a9b68ebb3ab95b4a2d3c6f58535e90dc
Nicolas Firmo <nicolasfpbarra@gmail.com>
2022-07-15 22:44:24 -0300

    Reorganization of the files structure.

---
+++ src/Utility/MathConstants.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace nic {
+
+	static constexpr auto E =		2.71828182845904523536;		// e
+	static constexpr auto LOG2E =	1.44269504088896340736;		// log2(e)
+	static constexpr auto LOG10E =	0.434294481903251827651;	// log10(e)
+	static constexpr auto LN2 =		0.693147180559945309417;	// ln(2)
+	static constexpr auto LN10 =	2.30258509299404568402;		// ln(10)
+	static constexpr auto PI =		3.14159265358979323846;		// pi
+	static constexpr auto SQRT2 =	1.41421356237309504880;		// sqrt(2)
+	static constexpr auto SQRT3 =	1.732050807568877293527;	// sqrt(3)
+
+} // namespace nic
